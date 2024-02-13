5ebb8bd0c6c4191959615004dc73dbe9829d80dc
ImplementLife <lmplementlifeg@gmail.com>
2024-02-13 22:39:40 +0200

    fix UI init exception

--- src/main/java/game/myStrategy/ui/menu/mainMenu/MainMenu.java
+++ src/main/java/game/myStrategy/ui/menu/mainMenu/MainMenu.java
@@ -163,27 +163,4 @@ public class MainMenu {
     public JComponent $$$getRootComponent$$$() {
         return root;
     }
-
-    /**
-     * @noinspection ALL
-     */
-    private Font $$$getFont1$$$(String fontName, int style, int size, Font currentFont) {
-        if (currentFont == null) return null;
-        String resultName;
-        if (fontName == null) {
-            resultName = currentFont.getName();
-        } else {
-            Font testFont = new Font(fontName, Font.PLAIN, 10);
-            if (testFont.canDisplay('a') && testFont.canDisplay('1')) {
-                resultName = fontName;
-            } else {
-                resultName = currentFont.getName();
-            }
-        }
-        Font font = new Font(resultName, style >= 0 ? style : currentFont.getStyle(), size >= 0 ? size : currentFont.getSize());
-        boolean isMac = System.getProperty("os.name", "").toLowerCase(Locale.ENGLISH).startsWith("mac");
-        Font fontWithFallback = isMac ? new Font(font.getFamily(), font.getStyle(), font.getSize()) : new StyleContext().getFont(font.getFamily(), font.getStyle(), font.getSize());
-        return fontWithFallback instanceof FontUIResource ? fontWithFallback : new FontUIResource(fontWithFallback);
-    }
-
 }
